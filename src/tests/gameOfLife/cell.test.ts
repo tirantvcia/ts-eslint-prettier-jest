0a61671c7a3ca6b722f631e607b9dd6bd649ffcd
tirantvcia <tirantvcia@gmail.com>
2024-02-01 07:07:47 +0000

    Any live cell with two or three live neighbors lives on to the next generation

--- src/tests/gameOfLife/cell.test.ts
+++ src/tests/gameOfLife/cell.test.ts
@@ -15,6 +15,12 @@ class Cell {
     }
 
     regenerate(numberOfNeighbors: number) {
+        if(this.status === CellStatus.Alive) {
+            if(numberOfNeighbors === 2 || numberOfNeighbors === 3){
+                return CellStatus.Alive;
+            }
+        }
+
         return CellStatus.Dead;    
     }
 }
@@ -24,5 +30,11 @@ describe('Game of Life', () => {
             const numberOfNeighbors = 1;
             expect(new Cell(CellStatus.Alive).regenerate(numberOfNeighbors)).toBe(CellStatus.Dead);
         })
-     
+        it('Any live cell with two or three live neighbors lives on to the next generation', ()=>{
+            let numberOfNeighbors = 2;
+            expect(new Cell(CellStatus.Alive).regenerate(numberOfNeighbors)).toBe(CellStatus.Alive);
+            numberOfNeighbors = 3;
+            expect(new Cell(CellStatus.Alive).regenerate(numberOfNeighbors)).toBe(CellStatus.Alive);
+            expect(new Cell(CellStatus.Dead).regenerate(numberOfNeighbors)).toBe(CellStatus.Dead);
+        })  
 });
