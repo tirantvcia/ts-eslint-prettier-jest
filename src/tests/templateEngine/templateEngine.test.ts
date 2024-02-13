bdffd924e323c6971afaac2887910fb8cc62c905
tirantvcia <tirantvcia@gmail.com>
2024-02-13 22:40:40 +0000

    refactor, add new  TemplateWarning and TemplateReplaced classes

--- src/tests/templateEngine/templateEngine.test.ts
+++ src/tests/templateEngine/templateEngine.test.ts
@@ -1,24 +1,38 @@
 /*
+Happy path:
 'This is a template with zero variables' -> 'This is a template with zero variables
 'This is a template with a ${variable}', {variable: 'food'} -> 'This is a template with food'
 'This is a template with a ${variable} and ${anotherVariable}', {variable: 'foo', anotherVariable: 'bar'} -> 'This is a template with a foo and bar'
+Edge cases:
+- Varoables not being found
+- Non replaced variables
+- Null text & null dictionary
 */
 
-import { setEngine } from "crypto"
 
 class TemplateEngine {
+
     constructor(private readonly template: string, private readonly variables: Map<string, string>){}
-    parse() {
-        if(this.variables.size == 0) {
-            return this.template;
-        }
+
+    parseNew() : TemplateReplaced {
         let textReplaced = this.template;
         this.variables.forEach((value, key) => {
             textReplaced = textReplaced.replaceAll('${'+key+'}', value);
         });
-        return textReplaced;
+        return new TemplateReplaced(textReplaced, [] );
     }
-    
+   
+}
+
+class TemplateReplaced {
+    constructor(readonly text: string, readonly warnings: ReadonlyArray<TemplateWarning>){}
+    containsWarnings () {
+        return this.warnings.length > 0;
+    }
+}
+
+class TemplateWarning {
+    constructor(readonly message: string) {}
 }
 
 describe('The template Engine', () => {
@@ -38,7 +52,7 @@ describe('The template Engine', () => {
         let variables = new Map<string, string>();
         variables.set('variable', 'food');
         const engine = new TemplateEngine(template, variables);
-        const parsedTemplate = engine.parse();
+        const parsedTemplate = engine.parseNew().text;
 
         expect(parsedTemplate).toEqual('This is a template with a food');
 
@@ -49,7 +63,7 @@ describe('The template Engine', () => {
         variables.set('variable', 'food');
         variables.set('anotherVariable', 'bar');
         const engine = new TemplateEngine(template, variables);
-        const parsedTemplate = engine.parse();
+        const parsedTemplate = engine.parseNew().text;
 
         expect(parsedTemplate).toEqual('This is a template with a food and bar');
 
@@ -61,9 +75,23 @@ describe('The template Engine', () => {
         variables.set('variable', 'food');
         variables.set('anotherVariable', 'bar');
         const engine = new TemplateEngine(template, variables);
-        const parsedTemplate = engine.parse();
+        const parsedTemplate = engine.parseNew().text;
 
         expect(parsedTemplate).toEqual('This is a template with a food, food and bar');
+
+    });
+
+    it('varoables not being found', () => {
+        const template = 'This is a template with a ${variable} found and another Variable not being found';
+        let variables = new Map<string, string>();
+        variables.set('variable', 'food');
+        variables.set('anotherVariable', 'bar');
+        const engine = new TemplateEngine(template, variables);
+        const parsedTemplate = engine.parseNew().text;
+        const isParseContainErrors = engine.containErrors();
+
+        expect(parsedTemplate).toEqual('This is a template with a food found and another Variable not being found');
+        expect(isParseContainErrors).toBeTruthy();
 
     });
 })
