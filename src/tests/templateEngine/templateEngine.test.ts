4055873be492c561d56a2d75eeab033e7bd85ab9
tirantvcia <tirantvcia@gmail.com>
2024-02-18 16:51:47 +0000

    Cubriendo nulos, refactornizar nombre método

--- src/tests/templateEngine/templateEngine.test.ts
+++ src/tests/templateEngine/templateEngine.test.ts
@@ -14,10 +14,20 @@ import { match } from "assert";
 
 class TemplateEngine {
 
-    constructor(private readonly template: string, private readonly variables: Map<string, string>){}
+    private constructor(private readonly template: string, private readonly variables: Map<string, string>){}
+
+    static create(template: string, variables: Map<string, string>) {
+        if((TemplateEngine.isNotValidate(template, variables)).length > 0) {
+            const checkedWarnings = TemplateEngine.isNotValidate(template, variables);
+            return new TemplateReplaced(template, checkedWarnings );
+        }
+
+        return new TemplateEngine(template, variables).parse();
+
+    }
 
 
-    parseNew() : TemplateReplaced {
+    private parse() : TemplateReplaced {
         let warnings:TemplateWarning[] = [];
         let textReplaced = this.template;
         this.variables.forEach((value, key) => {
@@ -29,9 +39,27 @@ class TemplateEngine {
         });
         const templateReplaced = new TemplateReplaced(textReplaced, warnings );
         return this.addWarningsAboutNonReplacedVariables(templateReplaced);
-
+    }
+    private static isNotValidate(template: string, variables: Map<string, string>) {
+        let warnings: TemplateWarning[] = [];
+        if(TemplateEngine.isVariablesNotValid(variables)) {
+            warnings.push(new TemplateWarning("Variables is not defined"));
+            
+        }
+        if(TemplateEngine.isTextNotValid(template)) {
+            warnings.push(new TemplateWarning("Text is not defined"));
+        }
+        return warnings;
+    }
+    private static isVariablesNotValid(variables: Map<string, string>) {
+        return !variables;
     }
 
+    static isTextNotValid(template: string) {
+        return !template;
+    }
+
+ 
     private addWarningsAboutNonReplacedVariables(templateReplaced:TemplateReplaced) {
         const textReplaced = templateReplaced.text;
         const warningsAboutNonReplacedVariables: TemplateWarning[] = [];
@@ -72,10 +100,10 @@ describe('The template Engine', () => {
         const template = 'This is a template with zero variables';
         let variables = new Map<string, string>();
 
-        const engine = new TemplateEngine(template, variables);
-        const parsedTemplate = engine.parseNew().text;
+        
+        const parsedTemplate = TemplateEngine.create(template, variables);
 
-        expect(parsedTemplate).toEqual(template);
+        expect(parsedTemplate.text).toEqual(template);
 
     })
     it('parse template with a variable', () => {
@@ -83,10 +111,10 @@ describe('The template Engine', () => {
 
         let variables = new Map<string, string>();
         variables.set('variable', 'food');
-        const engine = new TemplateEngine(template, variables);
-        const parsedTemplate = engine.parseNew().text;
 
-        expect(parsedTemplate).toEqual('This is a template with a food');
+        const parsedTemplate =  TemplateEngine.create(template, variables);
+
+        expect(parsedTemplate.text).toEqual('This is a template with a food');
 
     })
     it('parse template with more than one different variables', () => {
@@ -95,10 +123,10 @@ describe('The template Engine', () => {
         variables.set('variable', 'food');
         variables.set('anotherVariable', 'bar');
     
-        const engine = new TemplateEngine(template, variables);
-        const parsedTemplate = engine.parseNew().text;
 
-        expect(parsedTemplate).toEqual('This is a template with a food and bar');
+        const parsedTemplate =  TemplateEngine.create(template, variables);
+
+        expect(parsedTemplate.text).toEqual('This is a template with a food and bar');
 
     });
 
@@ -108,10 +136,9 @@ describe('The template Engine', () => {
         variables.set('variable', 'food');
         variables.set('anotherVariable', 'bar');
     
-        const engine = new TemplateEngine(template, variables);
-        const parsedTemplate = engine.parseNew().text;
+        const parsedTemplate =  TemplateEngine.create(template, variables);
 
-        expect(parsedTemplate).toEqual('This is a template with a food, food and bar');
+        expect(parsedTemplate.text).toEqual('This is a template with a food, food and bar');
 
     });
 
@@ -121,8 +148,7 @@ describe('The template Engine', () => {
         variables.set('variable', 'food');
         variables.set('anotherVariable', 'bar');
     
-        const engine = new TemplateEngine(template, variables);
-        const parsedTemplate = engine.parseNew();
+        const parsedTemplate =  TemplateEngine.create(template, variables);
       
         expect(parsedTemplate.text).toEqual('This is a template with a food found and another Variable not being found');
         expect(parsedTemplate.containsWarnings()).toBeTruthy();
@@ -134,11 +160,31 @@ describe('The template Engine', () => {
         let variables = new Map<string, string>();
         variables.set('variable', 'food');
 
-        const engine = new TemplateEngine(template, variables);
-        const parsedTemplate = engine.parseNew();
+        const parsedTemplate =  TemplateEngine.create(template, variables);
       
         expect(parsedTemplate.text).toEqual('Template with food and ${anotherVariable}');
         expect(parsedTemplate.containsWarnings()).toBeTruthy();
         expect(parsedTemplate.warnings[0].message).toEqual("variable anotherVariable not be replaced");
     });
+
+    it('warns about null dictionary', () => {
+        const template = '${variable} and ${anotherVariable}';
+        let variables = null;
+
+        const parsedTemplate =  TemplateEngine.create(template, variables);
+      
+        expect(parsedTemplate.text).toEqual('${variable} and ${anotherVariable}');
+        expect(parsedTemplate.containsWarnings()).toBeTruthy();
+        expect(parsedTemplate.warnings[0].message).toEqual("Variables is not defined");
+    });    
+    it('warns about null texxt', () => {
+        const template = null;
+        let variables = new Map<string, string>();
+
+        const parsedTemplate =  TemplateEngine.create(template, variables);
+      
+        expect(parsedTemplate.text).toBeNull();
+        expect(parsedTemplate.containsWarnings()).toBeTruthy();
+        expect(parsedTemplate.warnings[0].message).toEqual("Text is not defined");
+    });   
 })
