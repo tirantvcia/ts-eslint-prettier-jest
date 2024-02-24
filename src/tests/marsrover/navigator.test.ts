7fb91409ed49373d93507c376a992fdc8eabf9a7
tirantvcia <tirantvcia@gmail.com>
2024-02-24 11:36:11 +0000

    12.4. Envolviendo el mundo

--- src/tests/marsrover/navigator.test.ts
+++ src/tests/marsrover/navigator.test.ts
@@ -1,10 +1,20 @@
 export class Coordinates {
+    static boundaryLatitude: number = 10;
+    static boundaryLongitude: number = 10;
     private constructor(private readonly latitude: number, private readonly longitude: number){};
     
     public static create(latitude : number, longitude: number) {
         if(latitude < 0 || longitude < 0) {
             throw new Error("Values less than Zero not allowed");
         }
+
+        if(latitude >= Coordinates.boundaryLatitude) {
+            latitude = latitude % Coordinates.boundaryLatitude;
+        }
+        if(longitude >= Coordinates.boundaryLongitude) {
+            longitude = longitude % Coordinates.boundaryLongitude;
+        }
+
         return new Coordinates(latitude, longitude);
     }
 }
@@ -17,4 +27,18 @@ describe('The Coordinates', () => {
         expect(() => Coordinates.create(0, -1)).toThrow("Values less than Zero not allowed");
     
     });
+    it('wraps around latitude when reaching boundary' , () => {
+        let coordinates = Coordinates.create(10, 9);
+        expect(coordinates).toEqual(Coordinates.create(0, 9));
+        coordinates = Coordinates.create(11, 9);
+        expect(coordinates).toEqual(Coordinates.create(1, 9));
+    });
+    it('wraps around longitude when reaching boundary' , () => {
+        let coordinates = Coordinates.create(9, 10);
+        expect(coordinates).toEqual(Coordinates.create(9 , 0));
+        coordinates = Coordinates.create(9, 11);
+        expect(coordinates).toEqual(Coordinates.create(9, 1));
+    
+    });
+
 });
