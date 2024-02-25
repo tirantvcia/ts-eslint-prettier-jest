7bcba89bb03982ceea51d650a9dde7c368395337
tirantvcia <tirantvcia@gmail.com>
2024-02-25 16:14:38 +0000

    refactorizar  tests no válidos

--- src/tests/marsrover/navigator.test.ts
+++ src/tests/marsrover/navigator.test.ts
@@ -1,14 +1,6 @@
 import { Coordinates } from "../../core/marsrover/Coordinates";
 
 describe('The Coordinates', () => {
-    xit('Does not allowed values less than Zero for the latitude' , () => {
-        expect(() => Coordinates.create(-1, 0)).toThrow("Values less than Zero not allowed");
-    
-    });
-    xit('Does not allowed values less than Zero for the longitude' , () => {
-        expect(() => Coordinates.create(0, -1)).toThrow("Values less than Zero not allowed");
-    
-    });
     it('wraps around latitude when reaching boundary' , () => {
         let coordinates = Coordinates.create(10, 9);
         expect(coordinates).toEqual(Coordinates.create(0, 9));
