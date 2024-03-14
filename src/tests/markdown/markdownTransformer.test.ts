6c6d0eece30b18cdeb4cc23f90498a0b8207ecd3
tirantvcia <tirantvcia@gmail.com>
2024-03-14 20:03:34 +0000

    14.2 Lista de ejemplos y primeros pasos

--- src/tests/markdown/markdownTransformer.test.ts
+++ src/tests/markdown/markdownTransformer.test.ts
@@ -1,3 +1,13 @@
+/**
+ * Pasos para transformar el texto markdown
+ * - Buscar todos los links en el texto
+ * - Evitar los links duplicados
+ * - Almacenar los links en algún tipo de almacén
+ * - Transformar los links al formato achor
+ * - Reemplazar los links almacenados en el texto
+ * - Crear las notas al pie
+ * - Añadir las notas al pie
+ */
 export class TransformationMarkdownProcess {
     constructor(readonly markdownText:string){}
     transform() {
@@ -13,5 +23,13 @@ describe('The Markdown Transformer', ()=> {
         const transformedText = process.transform();
 
         expect(transformedText).toBe(markdownText);
+    })
+    xit('transform a given markdown text that contains one link', () => {
+        const markdownText = '[visible text link](url)';
+        const process:TransformationMarkdownProcess = new TransformationMarkdownProcess(markdownText);
+        const expectedTransformedText = 'visible text link [^anchor1] \n\n [^anchor1]: url';
+        const transformedText = process.transform();
+
+        expect(transformedText).toBe(expectedTransformedText);
     })
 })
